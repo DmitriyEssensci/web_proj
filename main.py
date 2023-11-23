410a099bf7a894cf3c68f55719d2441bf6ebbdef
dstarasov <dstarasov@rcud-rt.ru>
2023-11-24 00:28:22 +0300

    Create flasky project

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os
 from flask import Flask
+from flask import request
+from flask import make_response
+from flask import abort
+from flask import render_template
+from flask_bootstrap import Bootstrap5
+from flask_script import Manager
 
 def os_list_dir():
     dir = "C:\\Users\\sdgro\\OneDrive\\Рабочий стол\\Python\\web_proj\\data\\"
@@ -9,19 +15,36 @@ def os_list_dir():
     print(len(ls_input))
 
 app = Flask(__name__)
+
+#@app.route('/')
+# def index():
+#     return '<h1>Hello World!</h1>'
+
+# @app.route('/')
+# def index():
+#     user_agent = request.headers.get('User-Agent')
+    #return '<p>Your browser is %s</p>' % user_agent
+
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
-
+    response = make_response('<h1> This document carries a cookie! </h1>')
+    response.set_cookie('answer', '42')
+    #return response 
+    return render_template('index.html')
+ 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello %s! </h1>' % name
+    return render_template('user.html', name=name)
+
+@app.route('/user/<id>')
+def  get_user(id):
+    user = load_user(id)
+    if not user:
+        abort(404)
+        
+    return '<h1> Hello %s</h1> % user.name'
+    #return render_template('if-else_operation.html', id=user)
 
 if __name__ == '__main__':
     app.run(debug= True)
-
-
-
-
-
-os_list_dir()
+    Manager.run()
